45247f3e8bda7dc6c2cae193d2173925ad43bbf4
sowmya-narayan <sowmyakavi@gmail.com>
2014-01-07 15:28:28 +0530

    given production mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Sti::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.default_url_options = { :host => 'safe-eyrie-5937.herokuapp.com' }
+  
 end
